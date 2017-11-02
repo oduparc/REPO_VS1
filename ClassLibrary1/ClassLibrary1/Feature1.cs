b31d8ae89d80fd466641bd4378962c9debd6aaee
olivier duparc <olivier.duparc@atos.net>
2017-11-02 08:21:37 +0100

    Added func()

--- ClassLibrary1/ClassLibrary1/Feature1.cs
+++ ClassLibrary1/ClassLibrary1/Feature1.cs
@@ -8,9 +8,12 @@ namespace ClassLibrary1
 {
     class Feature1
     {
-        int x1 = 1;
-        int x2 = 2;
-        int sum = x1 + x2;
-        return sum;
+        public int Add()
+        {
+            int x1 = 1;
+            int x2 = 2;
+            int sum = x1 + x2;
+            return sum;
+        }
     }
 }
